ccbbd5020281e741f68c8e73793ce89dd62cfa09
Phathutshedzo <phathutshedzohmulaudzi@gmail.com>
2025-11-17 10:13:22 +0200

    Initial frontend commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { fileURLToPath } from 'url'
 
 // ESM replacement for __dirname
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 // https://vitejs.dev/config/
 export default defineConfig({
